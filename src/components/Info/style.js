958d7beb401efb7c2371bf5a976032499fec6e4d
Natália Serrão <nataliaserrao45@gmail.com>
2023-01-19 14:08:39 -0400

    feat: atualização read.me

--- src/components/Info/style.js
+++ src/components/Info/style.js
@@ -8,6 +8,16 @@ export const DivResults = styled.div`
     background-color: rgba(209, 220, 227, 0.48);
     padding-top: 3rem;
 
+    @media (min-width: 320px) and (max-width: 480px) {
+        width:50vw;
+        flex-direction: column;
+        background-color: var(--color-gray);
+        border-radius: 4px;
+        padding: 1rem;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+
+    }
+
     h3{
         font-family: 'Source Sans Pro', sans-serif;
         font-style: italic;
